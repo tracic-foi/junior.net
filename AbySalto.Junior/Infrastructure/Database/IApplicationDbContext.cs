b4ebd108398df613539a332b88be30e2e008ccc3
tracic-foi <tin.racic@gmail.com>
2026-04-30 18:48:35 +0200

    Add EF Core migrations and database setup

--- AbySalto.Junior/Infrastructure/Database/IApplicationDbContext.cs
+++ AbySalto.Junior/Infrastructure/Database/IApplicationDbContext.cs
@@ -1,6 +1,12 @@
-﻿namespace AbySalto.Junior.Infrastructure.Database
+﻿using AbySalto.Junior.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AbySalto.Junior.Infrastructure.Database
 {
     public interface IApplicationDbContext
     {
+        DbSet<Order> Orders { get; }
+        DbSet<OrderItem> OrderItems { get; }
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
     }
 }
